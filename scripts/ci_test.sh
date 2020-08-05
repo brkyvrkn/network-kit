cef7f32c3b8bdb80eb3595f8d1c4761849700fa4
Berkay Vurkan <berkayvurkan@yahoo.com>
2020-08-05 14:16:22 +0300

    bash modified

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -12,8 +12,9 @@ shift $((OPTIND -1))
 
 set -o pipefail
 
-xcodebuild build-for-testing -enableCodeCoverage YES -project "$project" -scheme "$scheme" -sdk $simulator -destination "${destinations[0]}" ONLY_ACTIVE_ARCH=YES | xcpretty;
+#xcodebuild build-for-testing -enableCodeCoverage YES -project "$project" -scheme "$scheme" -sdk $simulator -destination "${destinations[0]}" ONLY_ACTIVE_ARCH=YES | xcpretty;
 
 for destination in "${destinations[@]}"; do
-    xcodebuild test-without-building -enableCodeCoverage YES -project "$project" -scheme "$scheme" -sdk $simulator -destination "$destination" ONLY_ACTIVE_ARCH=YES | xcpretty;
+    xcodebuild test -enableCodeCoverage YES -project $project -scheme $scheme -sdk $simulator -destination "$destination" ONLY_ACTIVE_ARCH=YES | xcpretty;
+    #xcodebuild test -enableCodeCoverage YES -project "$project" -scheme "$scheme" -sdk $simulator -destination "$destination" ONLY_ACTIVE_ARCH=YES | xcpretty;
 done
